b64e8a12861151598cf4a8e6541b1ccc4b9bcf55
MaleneJensen1212 <112488753+MaleneJensen1212@users.noreply.github.com>
2025-12-31 09:53:24 +0100

    Add files via upload

---
+++ setup (2).sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#create virtual environment
+python3 -m venv .venv
+
+# activate venv
+source ./.venv/bin/activate
+
+# install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# close the environment
+deactivate
